11462bd2b71e4e8336d4cc8c4842910b9af94c41
eng-Zubr <eng.Zubr@gmail.com>
2016-05-24 01:26:02 +0300

    Update etcdbackupjob.sh

--- etcdbackupjob.sh
+++ etcdbackupjob.sh
@@ -33,5 +33,5 @@ EOF
 
 /root/etcd-backup -config=/root/backup-configuration.json \
   -etcd-config=/root/etcd-configuration.json dump && \
-  /root/gof3r cp --endpoint=${AWS_S3_ENDPOINT:-s3-eu-west-1.amazonaws.com} \
+  /root/gof3r cp --endpoint=${AWS_S3_ENDPOINT:-s3-us-east-1.amazonaws.com} \
   --debug /root/$DUMPFILE s3://${AWS_S3_BUCKET}/$DUMPFILE
